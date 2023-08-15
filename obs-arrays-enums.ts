1809f58836a7a7cbab22382ef4d32c45a1dd8d72
Leeyanhawrt <leeyan1108@gmail.com>
2023-08-15 15:53:53 -0400

    Typescript object arrays and enums

---
+++ obs-arrays-enums.ts
@@ -0,0 +1,37 @@
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string];
+// } = {
+//   name: "Leeyan",
+//   age: 25,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: "Leeyan",
+  age: 25,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+let favoriteActivites: string[];
+favoriteActivites = ["Sports"];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
+
+if (person.role === Role.AUTHOR) {
+  console.log("Hello Author");
+}
